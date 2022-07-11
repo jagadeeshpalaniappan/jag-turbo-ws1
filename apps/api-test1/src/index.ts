93ed6249c7b5a8984bfc0723ae4b2f60b0ec9840
Jagadeesh <jagadeeshthegeek@gmail.com>
2022-07-10 20:03:05 -0700

    u: src/index

--- apps/api-test1/src/index.ts
+++ apps/api-test1/src/index.ts
@@ -8,7 +8,7 @@ app.get("/api", (req, res) => {
   const path = `/api/item/1234`;
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end(`Hello -- v1! Go to item: <a href="${path}">${path}</a>`);
 });
 
 app.get("/api/item/:slug", (req, res) => {
