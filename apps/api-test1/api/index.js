ba6f3a3d90f81bbf1e7ec99d617f521a56504077
Jagadeesh <jagadeeshthegeek@gmail.com>
2022-07-10 20:01:32 -0700

    dist folder as api folder

--- apps/api-test1/api/index.js
+++ apps/api-test1/api/index.js
@@ -1,23 +1,19 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 // import cors from "cors"; // for CORS setup, usage: app.use(cors());
 var app = (0, express_1["default"])();
 app.get("/api", function (req, res) {
-  var path = "/api/item/1234";
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(
-    'Hello! Go to item: <a href="'.concat(path, '">').concat(path, "</a>")
-  );
+    var path = "/api/item/1234";
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+    res.end("Hello! Go to item: <a href=\"".concat(path, "\">").concat(path, "</a>"));
 });
 app.get("/api/item/:slug", function (req, res) {
-  var slug = req.params.slug;
-  res.end("Item: ".concat(slug));
+    var slug = req.params.slug;
+    res.end("Item: ".concat(slug));
 });
 module.exports = app;
