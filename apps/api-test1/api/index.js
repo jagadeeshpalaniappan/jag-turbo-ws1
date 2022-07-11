e9310db9f1aca076ca5a8fd7cf59fcd045ba5e22
Jagadeesh <jagadeeshthegeek@gmail.com>
2022-07-10 19:58:23 -0700

    u: api/index with dist/index

--- apps/api-test1/api/index.js
+++ apps/api-test1/api/index.js
@@ -1,16 +1,23 @@
-const app = require("express")();
-// const { v4 } = require("uuid");
-
-app.get("/api", (req, res) => {
-  const path = `/api/item/1234`;
+"use strict";
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
+// import cors from "cors"; // for CORS setup, usage: app.use(cors());
+var app = (0, express_1["default"])();
+app.get("/api", function (req, res) {
+  var path = "/api/item/1234";
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end(
+    'Hello! Go to item: <a href="'.concat(path, '">').concat(path, "</a>")
+  );
 });
-
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.get("/api/item/:slug", function (req, res) {
+  var slug = req.params.slug;
+  res.end("Item: ".concat(slug));
 });
-
 module.exports = app;
